f07487f599c3da25ac773e0ee6e86049f5d5f5ca
Ciuff4 <gasperini_cristina@libero.it>
2022-06-23 17:14:48 +0200

    print db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.main')
 @section('content')
-<p>ciao</p>
+<div class="container">
+    @foreach ( $comics as $comic)
+        <div class="card">
+            <img src="{{$comic->image}}" alt="">
+            <h3>{{$comic->title}}</h3>
+            <p>{{$comic->type}}</p>
+        </div>
+    @endforeach
+</div>
 @endsection
